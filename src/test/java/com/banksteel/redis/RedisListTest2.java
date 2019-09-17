27e844a8e2d63455b9af9fdb540964566751c174
wukaifeng <1076208637@qq.com>
2019-09-17 17:04:12 +0800

    list测试用例

--- src/test/java/com/banksteel/redis/RedisListTest2.java
+++ src/test/java/com/banksteel/redis/RedisListTest2.java
@@ -10,22 +10,22 @@ import redis.clients.jedis.SortingParams;
  */
 public class RedisListTest2 {
     private Jedis jedis = null;
-   
+
     @Before
     public void before() {
         jedis = new Jedis("localhost");
     }
-    
+
     @Test
     public void testList() {
-        System.out.println("======================list=========================="); 
-        jedis.select(0);
-        // 清空数据 
-        System.out.println("清空库中所有数据："+jedis.flushDB()); 
-  
+        System.out.println("======================list==========================");
+        jedis.select(4);
+        // 清空数据
+        System.out.println("清空库中所有数据：" + jedis.flushDB());
+
         System.out.println("=============增=============");
-        jedis.lpush("stringlists", "vector"); 
-        jedis.lpush("stringlists", "ArrayList"); 
+        jedis.lpush("stringlists", "vector");
+        jedis.lpush("stringlists", "ArrayList");
         jedis.lpush("stringlists", "vector");
         jedis.lpush("stringlists", "vector");
         jedis.lpush("stringlists", "LinkedList");
@@ -36,29 +36,29 @@ public class RedisListTest2 {
         jedis.lpush("numberlists", "1");
         jedis.lpush("numberlists", "5");
         jedis.lpush("numberlists", "2");
-        System.out.println("所有元素-stringlists："+jedis.lrange("stringlists", 0, -1));
-        System.out.println("所有元素-numberlists："+jedis.lrange("numberlists", 0, -1));
-        
+        System.out.println("所有元素-stringlists：" + jedis.lrange("stringlists", 0, -1));
+        System.out.println("所有元素-numberlists：" + jedis.lrange("numberlists", 0, -1));
+
         System.out.println("=============删=============");
         // 删除列表指定的值 ，第二个参数为删除的个数（有重复时），后add进去的值先被删，类似于出栈
-        System.out.println("成功删除指定元素个数-stringlists："+jedis.lrem("stringlists", 2, "vector")); 
-        System.out.println("删除指定元素之后-stringlists："+jedis.lrange("stringlists", 0, -1));
-        // 删除区间以外的数据 
-        System.out.println("删除下标0-3区间之外的元素："+jedis.ltrim("stringlists", 0, 3));
-        System.out.println("删除指定区间之外元素后-stringlists："+jedis.lrange("stringlists", 0, -1));
-        // 列表元素出栈 
-        System.out.println("出栈元素："+jedis.lpop("stringlists")); 
-        System.out.println("元素出栈后-stringlists："+jedis.lrange("stringlists", 0, -1));
-        
+        System.out.println("成功删除指定元素个数-stringlists：" + jedis.lrem("stringlists", 2, "vector"));
+        System.out.println("删除指定元素之后-stringlists：" + jedis.lrange("stringlists", 0, -1));
+        // 删除区间以外的数据
+        System.out.println("删除下标0-3区间之外的元素：" + jedis.ltrim("stringlists", 0, 3));
+        System.out.println("删除指定区间之外元素后-stringlists：" + jedis.lrange("stringlists", 0, -1));
+        // 列表元素出栈
+        System.out.println("出栈元素：" + jedis.lpop("stringlists"));
+        System.out.println("元素出栈后-stringlists：" + jedis.lrange("stringlists", 0, -1));
+
         System.out.println("=============改=============");
-        // 修改列表中指定下标的值 
-        jedis.lset("stringlists", 0, "hello list!"); 
-        System.out.println("下标为0的值修改后-stringlists："+jedis.lrange("stringlists", 0, -1));
+        // 修改列表中指定下标的值
+        jedis.lset("stringlists", 0, "hello list!");
+        System.out.println("下标为0的值修改后-stringlists：" + jedis.lrange("stringlists", 0, -1));
         System.out.println("=============查=============");
-        // 数组长度 
-        System.out.println("长度-stringlists："+jedis.llen("stringlists"));
-        System.out.println("长度-numberlists："+jedis.llen("numberlists"));
-        // 排序 
+        // 数组长度
+        System.out.println("长度-stringlists：" + jedis.llen("stringlists"));
+        System.out.println("长度-numberlists：" + jedis.llen("numberlists"));
+        // 排序
         /*
          * list中存字符串时必须指定参数为alpha，如果不使用SortingParams，而是直接使用sort("list")，
          * 会出现"ERR One or more scores can't be converted into double"
@@ -66,13 +66,13 @@ public class RedisListTest2 {
         SortingParams sortingParameters = new SortingParams();
         sortingParameters.alpha();
         sortingParameters.limit(0, 3);
-        System.out.println("返回排序后的结果-stringlists："+jedis.sort("stringlists",sortingParameters)); 
-        System.out.println("返回排序后的结果-numberlists："+jedis.sort("numberlists"));
-        // 子串：  start为元素下标，end也为元素下标；-1代表倒数一个元素，-2代表倒数第二个元素
-        System.out.println("子串-第二个开始到结束："+jedis.lrange("stringlists", 1, -1));
-        // 获取列表指定下标的值 
-        System.out.println("获取下标为2的元素："+jedis.lindex("stringlists", 2)+"\n");
+        System.out.println("返回排序后的结果-stringlists：" + jedis.sort("stringlists", sortingParameters));
+        System.out.println("返回排序后的结果-numberlists：" + jedis.sort("numberlists"));
+        // 子串： start为元素下标，end也为元素下标；-1代表倒数一个元素，-2代表倒数第二个元素
+        System.out.println("子串-第二个开始到结束：" + jedis.lrange("stringlists", 1, -1));
+        // 获取列表指定下标的值
+        System.out.println("获取下标为2的元素：" + jedis.lindex("stringlists", 2) + "\n");
 
     }
-    
+
 }
